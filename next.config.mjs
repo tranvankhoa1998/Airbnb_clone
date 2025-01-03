f9c2cef23872fffb650241999a71e774a64d22ca
tranvankhoa1998 <48855390+tranvankhoa1998@users.noreply.github.com>
2025-01-03 14:13:52 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
         port: "",
       },
       {
-        hostname: "glvmmupiqwlmhicmggqp.supabase.co",
+        hostname: "wyvdbgpclriftdfghwha.supabase.co",
         protocol: "https",
         port: "",
       },
